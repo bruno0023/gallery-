e6d2164820aba55e03e30d73d938a3509311e894
Bruno Alves de freitas <brunoalves-al@outlook.com>
2026-05-25 16:56:21 -0300

    feat: implementação de componentes gerais

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -4,11 +4,17 @@ export const Dialog = DialogPrimitive.Root;
 export const DialogTrigger = DialogPrimitive.Trigger;
 export const DialogClose = DialogPrimitive.Close;
 
-export function DialogContent({ className, ref, children, ...props }: React.ComponentProps<typeof DialogPrimitive.Content>) {
+export function DialogContent({
+    className,
+    ref,
+    children,
+    ...props
+}: React.ComponentProps<typeof DialogPrimitive.Content>) {
     return (
         <DialogPrimitive.Portal>
-            <DialogPrimitive.Content ref={ref} 
-            className={`
+            <DialogPrimitive.Content 
+                ref={ref}
+                className={`
                 fixed left-[50%] top-[50%] w-full max-w-lg
                 z-60 translate-x-[-50%] translate-y-[-50%]
 
