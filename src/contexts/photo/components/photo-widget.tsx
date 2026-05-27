e33bdbb535c36a036c546a318141b95fe42be927
Bruno Alves de freitas <brunoalves-al@outlook.com>
2026-05-27 13:19:01 -0300

    feat: add componente photo-widget

---
+++ src/contexts/photo/components/photo-widget.tsx
@@ -0,0 +1,71 @@
+import ImagiePreview from "../../../components/image-preview"
+import Text from "../../../components/text"
+import Skeleton from "../../../components/skeleton"
+import type { Photo } from "../models/photo"
+import Badge from "../../../components/badge"
+import { buttonTextVariants, buttonVariants} from "../../../components/button"
+import { Link } from "react-router"
+
+interface PhotoWidgetProps {
+    photo: Photo,
+    loading?: boolean
+}
+
+export default function PhotoWidget({
+    photo,
+    loading
+}: PhotoWidgetProps) {
+    return (
+        <div className="flex flex-col gap-4">
+            {!loading ? (
+                <ImagiePreview
+                    src={`/images/${photo.imageId}`}
+                    title={photo.title}
+                    imageClassName="w-54.25 h-54.25 rounded-lg"
+                />
+            ) : (
+                <Skeleton className="w-54.25 h-54.25 rounded-lg" />
+            )}
+
+            <div className="flex flex-col gap-2">
+                {!loading ? (
+                    <Text variant='paragraph-large' className='truncate'>{photo.title}</Text>
+                ) : (
+                    <Skeleton className="w-full h-6" />
+                )}
+
+                <div className='flex gap-2 min-h-5.5'>
+                    {!loading ? (
+                        <>
+                            {photo.albums.slice(0, 2).map(album => (
+                                <Badge className="truncate" size='xs' key={album.id}>
+                                    {album.title}
+                                </Badge>
+                            ))}
+                            {photo.albums.length > 2 &&
+                                <Badge size='xs'>
+                                    +{photo.albums.length - 2}
+                                </Badge>
+                            }
+                        </>
+                    ) : (
+                        <>
+                            {Array.from({ length: 2 }).map((_, index) => (
+                                <Skeleton key={`album-loading-${index}`} className="w-full h-5 rounded-sm" />
+                            ))}
+                        </>
+                    )}
+                </div>
+            </div>
+
+            {!loading ? (
+                <Link to={`/fotos/${photo.id}`} className={buttonVariants({ variant: 'secondary', className: 'py-2 px-2' })}>
+                    <Text className={buttonTextVariants({ variant: 'secondary', size: 'sm' })}>Detalhes da imagem</Text>
+                </Link>
+
+            ) : (
+                <Skeleton className="w-full h-10" />
+            )}
+        </div>
+    )
+}
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,7 +1,26 @@
-import Text from "../components/text";
+import Container from "../components/container";
+import PhotoWidget from "../contexts/photo/components/photo-widget";
+import type { Photo } from "../contexts/photo/models/photo";
 
 export default function PageHome() {
     return (
-        <Text variant='heading-medium'>Home</Text>
+        <Container>
+            <div className='grid grid-cols-4 gap-9'>
+                <PhotoWidget photo={
+                    {
+                        id: '123',
+                        title: 'Photo Title',
+                        imageId: 'portrait-tower.png',
+                        albums: [
+                            { id: '1', title: 'Album 1' },
+                            { id: '2', title: 'Album 2' },
+                            { id: '3', title: 'Album 3' }
+                        ]
+                    }}
+                />
+
+                <PhotoWidget photo={{} as Photo} loading={true} />
+            </div>
+        </Container>
     )
 }
